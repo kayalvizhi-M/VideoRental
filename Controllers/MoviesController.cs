e5ddd52c2f87cef70c08c9f331abcec4ddb79916
kayalvizhi <kayalvizhi.muthukumar21@gmail.com>
2022-09-20 11:59:23 -0700

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRental.Models;
+using VideoRental.ViewModels;
+
+namespace VideoRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Murder Mystery" };
+            var customers = new List<Customer>()
+            {
+                new Customer{Id = 1, Name = "cust1"},
+                new Customer{Id = 2, Name ="Cust2"}
+            };
+            var randomViewModel = new RandomViewModel
+            {
+                Movie = movie,
+                Customer = customers
+            };
+            return View(randomViewModel);
+            //return Content("Hello Kayal!");
+            //return HttpNotFound("OOPSEY!");
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("year :{0} and Month : {1}", year, month));
+
+        }
+    }
+}
